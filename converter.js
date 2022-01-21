e674917d91f0c6179bb5b2067c9313f1554bc351
J03Y004 <91735870+J03Y004@users.noreply.github.com>
2022-01-21 22:27:23 +0100

    modifiche varie

--- converter.js
+++ converter.js
@@ -1,5 +1,51 @@
 $(document).ready(function() {
 
+    var initCurrency = $("#select_currency_1").val();
+
+    var historicalValues = "https://freecurrencyapi.net/api/v2/historical?apikey=019e7eb0-7935-11ec-a3c5-bb07a300b220&base_currency=USD&date_from=2020-10-01&date_to=2022-01-21";
+    historicalValues = historicalValues + "&base_currency=" + initCurrency;
+    $.ajax({
+        contentType: "application/json",
+        type: "GET",
+        url: historicalValues,
+        crossdomain: true,
+        data: "",
+        dataType: "json",
+        success: function(asynCallResult) {
+            dati = asynCallResult.data;
+            const chart = LightweightCharts.createChart(document.getElementById('diagram'), {
+                width: 700,
+                height: 400,
+                layout: {
+                  backgroundColor: 'rgb(17, 17, 39)',
+                  textColor: 'rgba(255, 255, 255, 0.9)',
+                },
+                grid: {
+                  vertLines: {
+                    color: 'rgb(41, 44, 58)',
+                  },
+                  horzLines: {
+                    color: 'rgb(41, 44, 58)',
+                  },
+                },
+                crosshair: {
+                  mode: LightweightCharts.CrosshairMode.Normal,
+                },
+                priceScale: {
+                  borderColor: 'rgba(197, 203, 206, 0.8)',
+                },
+                timeScale: {
+                  borderColor: 'rgba(197, 203, 206, 0.8)',
+                },
+              });
+            const lineSeries = chart.addLineSeries();
+            lineSeries.setData(asynCallResult.data);
+        },
+        error: function() {
+
+        }
+    });
+
     $("#val_1").on("input", function() {
         var initCurrency = $("#select_currency_1").val();
         var finalCurrency = $("#select_currency_2").val();
