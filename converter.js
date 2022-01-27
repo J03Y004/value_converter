69b3d8afb747af30502f1f94032bd93a7c9c931f
J03Y004 <giojack200404@gmail.com>
2022-01-27 13:43:47 +0100

    graph updated

--- converter.js
+++ converter.js
@@ -1,27 +1,25 @@
 $(document).ready(function() {
-    
+
     //gets current date
     Date.prototype.toDateInputValue = (function() {
         var local = new Date(this);
         local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
-        return local.toJSON().slice(0,10);
+        return local.toJSON().slice(0, 10);
     });
 
-    document.getElementById('finalDate').value = "01-01-2017";
-    document.getElementById('finalDate').value = new Date().toDateInputValue();     //assigns current date to the final date input area
-
-    //historicalGraph();
+    document.getElementById('initDate').value = "2017-01-01";
+    document.getElementById('finalDate').value = new Date().toDateInputValue(); //assigns current date to the final date input area
 
     $("#val_1, #select_currency_1, #select_currency_2").on("input", function() {
-        let initCurrency = $("#select_currency_1").val();       //gets the input currency actually chosen
-        let finalCurrency = $("#select_currency_2").val();      //gets the output currency acctually chosen
-        let quantity = $("#val_1").val();                       //gests the quantity of money to convert
+        let initCurrency = $("#select_currency_1").val(); //gets the input currency actually chosen
+        let finalCurrency = $("#select_currency_2").val(); //gets the output currency acctually chosen
+        let quantity = $("#val_1").val(); //gests the quantity of money to convert
 
         let endpointapi = "https://freecurrencyapi.net/api/v2/latest?apikey=019e7eb0-7935-11ec-a3c5-bb07a300b220";
         endpointapi = endpointapi + "&base_currency=" + initCurrency;
         if (!(initCurrency == finalCurrency)) {
             let result = "";
-            $.ajax({        //asynchronous call
+            $.ajax({ //asynchronous call
                 contentType: "application/json",
                 type: "GET",
                 url: endpointapi,
@@ -31,7 +29,7 @@ $(document).ready(function() {
                 success: function(asynCallResult) {
                     result = quantity * asynCallResult.data[finalCurrency];
 
-                    if (result == 0)        //in order to estethical reasons when the upper input area is void I set to void (instead than 0) the bottom area too
+                    if (result == 0) //in order to estethical reasons when the upper input area is void I set to void (instead than 0) the bottom area too
                         result = null;
 
                     $("#val_2").val(result);
@@ -43,7 +41,7 @@ $(document).ready(function() {
         } else result = quantity;
     });
 
-    $("#initDate, #finalDate, #select_currency_1, #select_currency_2").on("input", function() {
+    $("#initDate, #finalDate, #select_currency_1, #select_currency_2").on("input", historicalGraph()/*function() {
         let initDate = $("#initDate").val();
         let finalDate = $("#finalDate").val();
         let initCurrency = $("#select_currency_1").val();
@@ -53,27 +51,57 @@ $(document).ready(function() {
         historicalValues = historicalValues + "&base_currency=" + initCurrency + "&date_from=" + initDate + "&date_to=" + finalDate;
 
         $.ajax({
-          contentType: "application/json",
-          type: "GET",
-          url: historicalValues,
-          crossdomain: true,
-          data: "",
-          dataType: "json",
-          success: function(asynCallResult) {
-              let dati = asynCallResult.data;
-              let dataGraph = [];
-              for (const data in dati) {
-                  dataGraph.push({time: data, value: dati[data][finalCurrency]})
-              }
-              generateGraph(dataGraph);
-          },
-          error: function() {
-              console.log("Freecurrency server not reachable");
-          }
-      });
-  
-    });
+            contentType: "application/json",
+            type: "GET",
+            url: historicalValues,
+            crossdomain: true,
+            data: "",
+            dataType: "json",
+            success: function(asynCallResult) {
+                let dati = asynCallResult.data;
+                let dataGraph = [];
+                for (const data in dati) {
+                    dataGraph.push({ time: data, value: dati[data][finalCurrency] })
+                }
+                generateGraph(dataGraph);
+            },
+            error: function() {
+                console.log("Freecurrency server not reachable");
+            }
+        });
+
+    }*/);
 });
+
+function historicalGraph() {
+    let initDate = $("#initDate").val();
+    let finalDate = $("#finalDate").val();
+    let initCurrency = $("#select_currency_1").val();
+    let finalCurrency = $("#select_currency_2").val();
+
+    let historicalValues = "https://freecurrencyapi.net/api/v2/historical?apikey=019e7eb0-7935-11ec-a3c5-bb07a300b220";
+    historicalValues = historicalValues + "&base_currency=" + initCurrency + "&date_from=" + initDate + "&date_to=" + finalDate;
+
+    $.ajax({
+        contentType: "application/json",
+        type: "GET",
+        url: historicalValues,
+        crossdomain: true,
+        data: "",
+        dataType: "json",
+        success: function(asynCallResult) {
+            let dati = asynCallResult.data;
+            let dataGraph = [];
+            for (const data in dati) {
+                dataGraph.push({ time: data, value: dati[data][finalCurrency] })
+            }
+            generateGraph(dataGraph);
+        },
+        error: function() {
+            console.log("Freecurrency server not reachable");
+        }
+    });
+}
 
 function generateGraph(dataGraph) {
     $("#diagram").empty();
@@ -111,35 +139,4 @@ function generateGraph(dataGraph) {
         const newRect = entries[0].contentRect;
         chart.applyOptions({ height: newRect.height, width: newRect.width });
     }).observe(document.getElementById("diagram"));
-
-    function historicalGraph() {
-        let initDate = $("#initDate").val();
-        let finalDate = $("#finalDate").val();
-        let initCurrency = $("#select_currency_1").val();
-        let finalCurrency = $("#select_currency_2").val();
-
-        let historicalValues = "https://freecurrencyapi.net/api/v2/historical?apikey=019e7eb0-7935-11ec-a3c5-bb07a300b220";
-        historicalValues = historicalValues + "&base_currency=" + initCurrency + "&date_from=" + initDate + "&date_to=" + finalDate;
-
-        $.ajax({
-          contentType: "application/json",
-          type: "GET",
-          url: historicalValues,
-          crossdomain: true,
-          data: "",
-          dataType: "json",
-          success: function(asynCallResult) {
-              let dati = asynCallResult.data;
-              let dataGraph = [];
-              for (const data in dati) {
-                  dataGraph.push({time: data, value: dati[data][finalCurrency]})
-              }
-              generateGraph(dataGraph);
-          },
-          error: function() {
-              console.log("Freecurrency server not reachable");
-          }
-      });
-  
-    }
 }
